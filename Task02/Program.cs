b5e846785ea4df702443b1ac9553c6a9fdca13c2
Ivan <newzorin@gmail.com>
2022-10-27 12:22:13 +0400

    bigORsmoll1

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число");
+
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2)
+{
+    Console.WriteLine("числа ровны");
+}
+
+else
+{
+
+
+
+if (number1 > number2)
+{
+    Console.WriteLine($"число {number1} больше числа {number2} ");
+}
+
+else
+
+{
+    Console.WriteLine($"число {number2} больше числа {number1} ");
+}
+
+}
