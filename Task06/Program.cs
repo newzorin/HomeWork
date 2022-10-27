cb05ab89d75d66c6e7e02187e3f996d38282a8c7
Ivan <newzorin@gmail.com>
2022-10-27 19:26:40 +0400

    Task02_2

---
+++ Task06/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int number2 = number1 % 2;
+
+if (number2 > 0)
+{
+    Console.WriteLine($"число {number1} не четное");
+}
+
+else
+{
+    Console.WriteLine($"число {number1} четное");
+}
+
+
+
+
