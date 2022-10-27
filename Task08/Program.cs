eebf89be61c44c1cfff18e4332abbda1fe743196
Ivan <newzorin@gmail.com>
2022-10-27 20:46:27 +0400

    Task08_3

---
+++ Task08/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int Index = 0;
+
+while (Index <= number1)
+{
+    if (Index % 2 > 0)
+    {
+      Console.Write("");
+    }
+
+     else
+    {
+       Console.Write($"{Index},");
+    }
+   
+    Index++;
+}
+
+
+
