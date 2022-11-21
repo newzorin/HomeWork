acc569678f46f5b4a36a0680e5584d5971a666b4
Ivan <newzorin@gmail.com>
2022-11-21 23:32:22 +0400

    HWhomework

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} |");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+int FindNumMatrix(int[,] matrix, int rows, int columns)
+{
+    int res = matrix[rows, columns];
+    return res;
+}
+
+Console.WriteLine("Введите позицию i искомого элемента");
+int i1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите позицию j искомого элемента");
+int j1 = Convert.ToInt32(Console.ReadLine());
+
+int[,] array1 = CreateMatrixRndInt(5, 5, 0, 10);
+PrintMatrix(array1);
+
+if (i1 <= 4 && j1 <= 4)
+{
+   int result = FindNumMatrix(array1, i1, j1);
+   Console.WriteLine($"Искомые элемент массива = {result} ");
+}  
+else
+{
+    Console.WriteLine($"Элемент не найден");
+}
---
+++ Task52/Program.cs
@@ -0,0 +1,60 @@
+﻿int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} |");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+Double[] SumMatrixRndInt(int[,] matrix)
+{
+    int a = matrix.GetLength(1);
+    Double[] array = new Double[a]; 
+    int sum = 0;
+    for (int j = 0; j < matrix.GetLength(1) + 1; j++)
+    {
+        array[j] = sum / 3;
+        sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum = sum + matrix[i, j];
+        }
+
+    }
+    return array;
+}
+
+void PrintArray(Double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int[,] arraymatrix1 = CreateMatrixRndInt(3, 4, 0, 9);
+PrintMatrix(arraymatrix1);
+Double[] result = SumMatrixRndInt(arraymatrix1);
+PrintArray(result);
